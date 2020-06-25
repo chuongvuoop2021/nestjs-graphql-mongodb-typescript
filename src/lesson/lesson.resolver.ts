593199acc8ed4efcd73ca5833743c595e582e95c
chuongvuoop2021 <chuongvuoop2021@gmail.com>
2020-06-25 18:44:13 +0300

    First Mutation

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,20 @@
+import { Resolver, Query, Mutation } from '@nestjs/graphql'
+import { LessonType } from './lesson.type'
+
+@Resolver(of => LessonType)
+export class LessonResolver {
+  @Query(returns => LessonType)
+  lesson() {
+    return {
+      id: 'randomstring',
+      name: 'Class Name',
+      startDate: (new Date()).toISOString(),
+      endDate: (new Date()).toISOString()
+    }
+  }
+
+  @Mutation(returns => LessonType)
+  createLesson() { 
+
+  }
+}
